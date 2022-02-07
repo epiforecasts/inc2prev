10dbee3963433ac4bd54bb3c5b813533bd9fd5aa
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2022-02-07 08:05:45 +0000

    add update script

---
+++ scripts/update.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Rscript data-raw/update-cis.R
+
+echo Space
+Rscript scripts/estimate.R -n -d 1
+Rscript scripts/analyse.R
+
+echo Age
+Rscript scripts/estimate.R -g -d 1
+Rscript scripts/analyse.R -g
+
+echo Variants
+Rscript scripts/estimate.R -v -d 1
+Rscript scripts/analyse.R -v
