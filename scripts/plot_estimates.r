dd0ed114f6b4bdad2049b167f02135a5a5702f5c
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2022-02-28 17:31:04 +0000

    more plots

--- scripts/plot_estimates.r
+++ scripts/plot_estimates.r
@@ -7,6 +7,10 @@ library(readr)
 library(scales)
 library(tidyr)
 
+## whether to make spaghetti plots
+spaghetti <- FALSE
+
+## Get tools
 source(here::here("R", "read.R"))
 prev <- read_cis()
 local_region <- prev %>%
@@ -16,49 +20,6 @@ local_region <- prev %>%
 
 dir.create(here::here("figures", "additional"),
            showWarnings = FALSE, recursive = TRUE)
-
-## Get tools
-files <-
-  list.files(here("outputs"), pattern = "estimates_[^_]+\\.csv", full.names = TRUE)
-data <- map(files, read_csv) %>%
-  bind_rows() %>%
-  separate(variable, c("variable", "geography"), sep = "\\|")
-data <- left_join(data, local_region, by = c("level", "variable")) %>%
-  pivot_longer(starts_with("q"), names_to = "quantile") %>%
-  mutate(value = if_else(name == "infections", value / population, value)) %>%
-  pivot_wider(names_from = "quantile")
-
-cum_files <-
-  list.files(here("outputs"), pattern = "cumulative_[^_]+\\.rds", full.names = TRUE)
-if (length(cum_files) > 0) {
-  cum_data <- map(cum_files, readRDS) %>%
-    bind_rows() %>%
-    group_by(name, date, variable, variant, level) %>%
-    summarise(
-      q5 = quantile(value, 0.05),
-      q95 = quantile(value, 0.95),
-      q25 = quantile(value, 0.25),
-      q75 = quantile(value, 0.75),
-      q45 = quantile(value, 0.45),
-      q55 = quantile(value, 0.55),
-      .groups = "drop"
-    ) %>%
-    mutate(geography = if_else(
-             !is.na(variant),
-             as.character(variable),
-             NA_character_
-           ),
-           variable = if_else(
-             is.na(variant),
-             as.character(variable),
-             variant))
-  data <- data %>%
-    bind_rows(cum_data)
-}
-
-data <- data %>%
-  mutate(variable = if_else(variable == "2-10", "02-10", variable),
-         name = if_else(name == "R", "Rt", name))  
 
 var_names <- c(
   est_prev = "Prevalence estimate",
@@ -93,80 +54,107 @@ hline <- c(
   Rt = 1
 )
 
-non_variant <- data %>%
-  filter(!grepl("^variant", level))
-
 histories <- list(all = Inf, `1year` = months(12), `3months` = months(3))
 breaks <- c(all = "4 months", `1year` = "3 month", `3months` = "1 month")
 
-for (history in names(histories)) {
-  for (level in unique(non_variant$level)) {
+file_pattern <-
+  paste0(ifelse(spaghetti, "samples", "estimates"), "_[^_]+\\.rds")
+
+files <-
+  list.files(here("outputs"),
+	     pattern = file_pattern,
+	     full.names = TRUE)
+
+for (file in files) {
+  data <- readRDS(file) %>%
+    separate(variable, c("variable", "geography"), sep = "\\|")
+  cum_file <-
+    here::here("outputs", paste0("cumulative_", sub("^[^_]+_", "", file)))
+  if (file.exists(cum_file)) {
+    cum_data <- readRDS(cum_file) %>%
+      filter(name %in% names(var_names))
+    if (any(grepl("^variant", unique(data$level)))) {
+      cum_data <- cum_data %>%
+        mutate(geography = as.character(variable),
+               variable = variant)
+    }
+    if (!spaghetti) {
+      quantiles <- parse_number(grep("^q", colnames(data), value = TRUE)) / 100
+      cum_data <- cum_data %>%
+        group_by_at(vars(intersect(colnames(data), colnames(.)))) %>%
+        summarise(
+          value = quantile(value, quantiles),
+          q = paste0("q", quantiles * 100),
+          .groups = "drop"
+        ) %>%
+        pivot_wider(names_from = "q")
+    }
+    data <- data %>%
+      bind_rows(cum_data)
+    if (!"geography" %in% colnames(data)) {
+      data <- data %>%
+        mutate(geography = NA_character_,
+               variable = as.character(variable))
+    }
+  }
+
+  for (level in unique(data$level)) {
+    level_data <- data %>%
+      filter(level == {{ level }}) %>%
+      mutate(variable = if_else(variable == "2-10", "02-10", variable),
+             name = if_else(name == "R", "Rt", name)) %>%
+      filter(name %in% names(var_names))
+
+    if (level == "local") {
+      level_data <- level_data %>%
+        left_join(local_region, by = c("level", "variable"))
+    }
+
     colour_var <- ifelse(level == "local", "region", "variable")
-    for (name in unique(non_variant$name)) {
-      p <- ggplot(non_variant %>%
-                  filter(level == {{ level }},
-                         name == {{ name }},
-                         date > max(date) - histories[[history]]),
-                  aes_string(x = "date",
-			     colour = colour_var, fill = colour_var,
-			     group = "variable")) +
-        geom_ribbon(mapping = aes(ymin = `q45`, ymax = `q55`), alpha = 0.5) +
-        geom_ribbon(mapping = aes(ymin = `q25`, ymax = `q75`), alpha = 0.25) +
-        geom_ribbon(mapping = aes(ymin = `q5`, ymax = `q95`), alpha = 0.125) +
-        ylab(name) + xlab("") +
-        scale_x_date(breaks = breaks[[history]],
-                     labels = date_format("%b %Y")) +
-        scale_y_continuous(var_names[name], labels = labels[[name]]) +
-        scale_colour_brewer(group[level],  palette = "Set1") +
-        scale_fill_brewer(group[level], palette = "Set1") +
-        theme_minimal()
-      if (name %in% names(hline)) {
-        p <- p + geom_hline(yintercept = hline[[name]], linetype = "dashed")
+
+    for (history in names(histories)) {
+      for (name in unique(level_data$name)) {
+        plot_df <- level_data %>%
+          filter(name == {{ name }}, 
+		 date > max(date) - histories[[history]])
+        aes_str <- list(x = "date", colour = colour_var)
+        if (spaghetti) {
+          plot_df <- plot_df %>%
+            mutate(var_sample = interaction(variable, sample))
+          aes_str <- c(aes_str, list(y = "value", group = "var_sample"))
+        } else {
+          aes_str <- c(aes_str, list(fill = colour_var))
+        }
+        p <- ggplot(plot_df, do.call(aes_string, aes_str)) +
+          ylab(name) + xlab("")
+        if (spaghetti) {
+          p <- p +
+            geom_line(alpha = 0.25)
+        } else {
+          p <- p +
+            geom_ribbon(mapping = aes(ymin = `q45`, ymax = `q55`), alpha = 0.5) +
+            geom_ribbon(mapping = aes(ymin = `q5`, ymax = `q95`), alpha = 0.125)
+        }
+        p <- p +
+          scale_x_date(breaks = breaks[[history]],
+                       labels = date_format("%b %Y")) +
+          scale_y_continuous(var_names[name], labels = labels[[name]]) +
+          scale_colour_brewer(group[level],  palette = "Set1") +
+          scale_fill_brewer(group[level],  palette = "Set1") +
+          theme_minimal()
+        if (name %in% names(hline)) {
+          p <- p +
+            geom_hline(yintercept = hline[[name]], linetype = "dashed")
+        }
+        if (grepl("^variant_", level)) {
+          p <- p +
+            facet_wrap(~geography)
+        }
+        ggsave(here::here("figures", "additional",
+                          paste0(level, "_", name, "_",
+                                 history, ".svg")), p,
+               width = 10, height = 5)
       }
-      ggsave(here::here("figures", "additional",
-			paste0(level, "_", name, "_",
-                               history, ".svg")), p,
-             width = 10, height = 5)
     }
   }
 }
-
-variant <- data %>%
-  filter(grepl("^variant", level))
-
-for (history in names(histories)) {
-  for (level in unique(variant$level)) {
-    for (name in unique(variant$name)) {
-      p <- ggplot(
-        variant %>%
-          filter(
-            level == {{ level }},
-            name == {{ name }}
-          ),
-        aes(x = date, colour = variable, fill = variable)
-      ) +
-        geom_ribbon(mapping = aes(ymin = `q45`, ymax = `q55`), alpha = 0.5) +
-        geom_ribbon(mapping = aes(ymin = `q25`, ymax = `q75`), alpha = 0.25) +
-        geom_ribbon(mapping = aes(ymin = `q5`, ymax = `q95`), alpha = 0.125) +
-        ylab(name) +
-        xlab("") +
-        scale_x_date(
-          breaks = breaks[[history]],
-          labels = date_format("%b %Y")
-        ) +
-        scale_y_continuous(var_names[name], labels = labels[[name]]) +
-        scale_colour_brewer(group[level], palette = "Set1") +
-        scale_fill_brewer(group[level], palette = "Set1") +
-        theme_minimal() +
-        facet_wrap(~geography)
-      if (name %in% names(hline)) {
-        p <- p + geom_hline(yintercept = hline[[name]], linetype = "dashed")
-      }
-      ggsave(here::here("figures", "additional",
-			paste0(level, "_", name, ".svg")), p,
-        width = 14, height = 8
-      )
-    }
-  }
-}
-
