1339b240a3057d90f2add9212e01b78f76556d0b
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2022-05-20 13:02:03 +0100

    also process higher ab threshold

--- scripts/process_dates.sh
+++ scripts/process_dates.sh
@@ -5,3 +5,8 @@ while read line; do
   Rscript scripts/estimate.R -d 1 -g -a -m $line
 done < scripts/dates
 
+while read line; do
+  echo standard $line
+  Rscript scripts/estimate.R -d 1 -g -a -i -m $line
+done < scripts/dates
+
