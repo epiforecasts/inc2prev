a4c0d4a4c91b3cf4964037c9411d06454ea2728a
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2022-05-28 18:38:46 +0100

    update initial values

--- R/model.R
+++ R/model.R
@@ -264,11 +264,11 @@ i2p_inits <- function(dat) {
 
     if (dat$ab_obs > 0) {
       init_list <- c(init_list, list(
-        beta = array(inv_logit(rnorm(1, 2, 0.4))),
-        gamma = array(inv_logit(rnorm(2, -9, 0.4))),
-        delta = array(inv_logit(rnorm(1, 3, 0.4))),
-        k = array(exp(rnorm(1, 0, 0.1))),
-        l = array(exp(rnorm(1, 0, 0.1))),
+        logit_beta = array(rnorm(1, 2, 0.4)),
+        logit_gamma = array(rnorm(2, -9, 0.4)),
+        logit_delta = array(rnorm(1, 3, 0.4)),
+        k = array(rlnorm(1, 0, 0.1)),
+        l = array(rlnorm(1, 0, 0.1)),
         ab_sigma = array(
           truncnorm::rtruncnorm(1, mean = 0.005, sd = 0.0025, a = 0)
         ),
