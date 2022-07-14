f57774f6a80cf46cbfb4e0b7bb7c3d249a66c364
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2022-07-14 14:00:20 +0100

    remove comment

--- scripts/local_snapshots.sh
+++ scripts/local_snapshots.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-## echo Delta
-## . ./scripts/local.sh -m 2021-04-01
+echo Delta
+. ./scripts/local.sh -m 2021-04-01
 echo Omicron BA.1
 . ./scripts/local.sh -m 2021-11-01
 echo Omicron BA.2
