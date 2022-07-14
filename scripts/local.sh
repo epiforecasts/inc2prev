0bb03024887375f176203e829ad96a50e6172bf8
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2022-07-14 12:07:33 +0000

    local snapshots

--- scripts/local.sh
+++ scripts/local.sh
@@ -6,9 +6,10 @@ Rscript data-raw/update_cis.R
 Rscript data-raw/update_ab.R
 
 echo Local
-Rscript scripts/estimate.R -d 1 -l && Rscript scripts/analyse.R -l
+Rscript scripts/estimate.R -d 1 -l $* && Rscript scripts/analyse.R -l $*
 
 echo Regional
-git status | grep -q cis.csv && Rscript scripts/estimate.R -r -d 1 -i -a && Rscript scripts/analyse.R -r -a
+Rscript scripts/estimate.R -r -d 1 -i -a $* && Rscript scripts/analyse.R -r -a -i $*
+Rscript scripts/estimate.R -r -d 1 -a $* && Rscript scripts/analyse.R -r -a $*
 
-Rscript scripts/generate_local_ab.r
+Rscript scripts/generate_local_ab.r $*
---
+++ scripts/local_snapshots.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+## echo Delta
+## . ./scripts/local.sh -m 2021-04-01
+echo Omicron BA.1
+. ./scripts/local.sh -m 2021-11-01
+echo Omicron BA.2
+. ./scripts/local.sh -m 2021-12-06
