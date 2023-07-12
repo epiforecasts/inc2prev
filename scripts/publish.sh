359e41abb2d93e96d764b8af17ba2b974a109e97
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2023-07-12 22:10:02 +0100

    update publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,14 +3,6 @@
 git pull -Xours
 git add data-processed/*.csv
 git add outputs/*.csv
+git add pkgdown/assets
 git diff-index --quiet HEAD || git commit -m"Updated estimates $(date)"
 git push -v
-
-git branch -D gh-pages
-git checkout --orphan gh-pages
-git rm -rf .
-git add docs/*.html
-git commit -m "Update report"
-git push --force origin gh-pages
-git checkout master
-
